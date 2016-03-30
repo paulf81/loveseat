5d9b2508b6b4c161d9b21c3f0fc9323d1fe18ddc
Matt Churchfield <matt.churchfield@nrel.gov>
2016-03-30 08:03:40 -0600

    Additions to the meso/micro version of ABLSolver. -- M. Churchfield -- 30 March 2016

--- applications/solvers/incompressible/windEnergy/ABLSolver.ext/interpolate2D/interpolate2D.C
+++ applications/solvers/incompressible/windEnergy/ABLSolver.ext/interpolate2D/interpolate2D.C
@@ -1,14 +1,4 @@
 /*---------------------------------------------------------------------------*\
-This file was modified or created at the National Renewable Energy
-Laboratory (NREL) on March 29, 2016 in creating the SOWFA (Simulator for
-Offshore Wind Farm Applications) package of wind plant modeling tools that
-are based on the OpenFOAM software. Access to and use of SOWFA imposes
-obligations on the user, as set forth in the NWTC Design Codes DATA USE
-DISCLAIMER AGREEMENT that can be found at
-<http://wind.nrel.gov/designcodes/disclaimer.html>.
-\*---------------------------------------------------------------------------*/
-
-/*---------------------------------------------------------------------------*\
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
@@ -34,28 +24,98 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "interpolate2D.H"
+#include "primitiveFields.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
-
-interpolate2D:interpolate2D
+template<class Type>
+Field<Type> interpolate2D
 (
+    const scalarField& xi,
+    const scalarField& yi,
+    const scalarField& x,
+    const scalarField& y,
+    const Field<Type>& f
 )
-:
-
 {
+    Field<Type> yNew(xNew.size());
+
+    forAll(xNew, i)
+    {
+        yNew[i] = interpolate2D(xNew[i], xOld, yOld);
+    }
+
+    return yNew;
 }
 
 
-// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
-
-scalar intepolate2D:evaluate()
+template<class Type>
+Type interpolate2D
+(
+    const scalar xi,
+    const scalar yi,
+    const scalarField& x,
+    const scalarField& y,
+    const Field<Type>& f
+)
 {
+    label n = xOld.size();
+
+    label lo = 0;
+    for (lo=0; lo<n && xOld[lo]>x; ++lo)
+    {}
+
+    label low = lo;
+    if (low < n)
+    {
+        for (label i=low; i<n; ++i)
+        {
+            if (xOld[i] > xOld[lo] && xOld[i] <= x)
+            {
+                lo = i;
+            }
+        }
+    }
+
+    label hi = 0;
+    for (hi=0; hi<n && xOld[hi]<x; ++hi)
+    {}
+
+    label high = hi;
+    if (high < n)
+    {
+        for (label i=high; i<n; ++i)
+        {
+            if (xOld[i] < xOld[hi] && xOld[i] >= x)
+            {
+                hi = i;
+            }
+        }
+    }
+
+
+    if (lo<n && hi<n && lo != hi)
+    {
+        return yOld[lo]
+            + ((x - xOld[lo])/(xOld[hi] - xOld[lo]))*(yOld[hi] - yOld[lo]);
+    }
+    else if (lo == hi)
+    {
+        return yOld[lo];
+    }
+    else if (lo == n)
+    {
+        return yOld[hi];
+    }
+    else
+    {
+        return yOld[lo];
+    }
 }
 
 
